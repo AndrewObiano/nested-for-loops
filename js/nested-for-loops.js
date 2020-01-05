542fa0606af8418eeef01dceccc93eafcdfa279d
Andrew Obiano <andrew.obiano@gmail.com>
2020-01-04 15:43:12 -1000

    finished

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -1,29 +1,23 @@
 /**
- * Returns a string representation of a 2-dimensional data structure 
- * @param {number} depth 
- * @param {number} [width=depth] 
+ * Returns a string representation of a 2-dimensional data structure
+ * @param {number} depth
+ * @param {number} [width=depth]
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
+function nestedForLoops(depth, width = depth) {
   let result = "\n"; // "NEW LINE"
 
   let arr = [];
 
-  for(a=0; a<depth; a++){
+  for (let a = 0; a < depth; a++) {
     arr[a] = [];
-    for(b=0; b<width; b++){
-      arr[a][b] = '{x:' + b + ', y:' + a + '}';
-      if(b<width && b>0){
-        arr[a][b] = ' ' + arr[a][b];
+    for (let b = 0; b < width; b++) {
+      arr[a][b] = "{x:" + b + ", y:" + a + "}";
+      if (b < width && b > 0) {
+        arr[a][b] = " " + arr[a][b];
       }
-      arr.join(',');
-      
+      arr.join(",");
     }
-  };
+  }
   return result + arr.join(result) + result;
-
-};
-
-// To see your console output outside the tests add function calls here.
-//console.log(nestedForLoops(4));
-console.log(nestedForLoops(3));
+}
